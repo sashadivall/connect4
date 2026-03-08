5e6617bf82acffe9297d8cab69076ba382beab9d
Sasha DiVall <sashadivall@gmail.com>
2026-03-08 16:02:16 -0700

    initial gameplay

---
+++ src/connect4_game.py
@@ -0,0 +1,31 @@
+from connect4_gui import *
+
+
+class Connect4Game:
+    def __init__(self):
+        # think about what variables we need to track
+        # player turn? 
+        # we should structure this similar to hw1 - have a different file handle the AI stuff
+        pass
+
+
+    def get_valid_moves(self):
+        """
+        Retrieves all legal moves from the current board state
+        """
+        # think about how we want to visually handle users choosing the wrong move
+        pass
+
+    def drop_piece(self):
+        """
+        Drops a piece corresponding to the current player in the 
+        chosen move
+        """
+        pass
+
+    def check_win(self):
+        """
+        Checks if, after the most recent move, a player has won
+        """
+        pass 
+
