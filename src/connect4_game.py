a512b1d3a1bcc45d87f521d457fdd95898af1516
Sasha DiVall <sashadivall@gmail.com>
2026-03-10 11:49:22 -0400

    drop_piece function complete with testing

--- src/connect4_game.py
+++ src/connect4_game.py
@@ -2,10 +2,12 @@ from connect4_gui import *
 
 
 class Connect4Game:
-    def __init__(self, board):
+    def __init__(self, rows, cols, board):
         # think about what variables we need to track
         # player turn? 
         # we should structure this similar to hw1 - have a different file handle the AI stuff
+        self.rows = rows
+        self.cols = cols 
         self.board = board
         self.player_turn = 1  # player 1 starts
         self.won = False
@@ -18,13 +20,30 @@ class Connect4Game:
         # think about how we want to visually handle users choosing the wrong move
         pass
 
-    def drop_piece(self):
+    def _is_valid_move(self, col):
+        return self.board[0][col] == 0
+
+
+
+    def drop_piece(self, col):
         """
         Drops a piece corresponding to the current player in the 
         chosen move 
         Updates the board with the current players value 
         """
-        pass
+
+        if self._is_valid_move(col):
+            # find the most recently empty row 
+            i = self.board.shape[0] - 1
+            while i >= 0:
+                cur_cell = self.board[i][col]
+                if cur_cell == 0:
+                    self.board[i][col] = self.player_turn
+                    break
+                else:
+                    i -= 1
+        # change player move 
+        self.player_turn = (self.player_turn % 2) + 1
 
     def check_win(self):
         """
@@ -33,3 +52,34 @@ class Connect4Game:
         """
         pass 
 
+
+def main():
+    rows = 6
+    cols = 7
+    board = np.zeros((rows, cols))
+    game = Connect4Game(rows, cols, board)
+
+    game.drop_piece(0)
+    print(game.board)
+
+    game.drop_piece(2)
+    print(game.board)
+
+    game.drop_piece(0)
+    print(game.board)
+
+    game.drop_piece(1)
+    print(game.board)
+
+    game.drop_piece(0)
+    print(game.board)
+
+    game.drop_piece(2)
+    print(game.board)
+
+    game.drop_piece(0)
+    print(game.board)
+
+if __name__ == '__main__':
+    main()
+    
